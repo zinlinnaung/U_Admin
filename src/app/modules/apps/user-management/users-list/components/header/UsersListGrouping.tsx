22f7221afcba956bcc0c0078aa40469fa7707480
Zin Linn Aung <Zinlinaung4905@gmail.com>
2025-10-30 21:52:02 +0630

    updated

--- src/app/modules/apps/user-management/users-list/components/header/UsersListGrouping.tsx
+++ src/app/modules/apps/user-management/users-list/components/header/UsersListGrouping.tsx
@@ -1,38 +1,38 @@
-import {useQueryClient, useMutation} from '@tanstack/react-query'
-import {QUERIES} from '../../../../../../../_metronic/helpers'
-import {useListView} from '../../core/ListViewProvider'
-import {useQueryResponse} from '../../core/QueryResponseProvider'
-import {deleteSelectedUsers} from '../../core/_requests'
+// import {useQueryClient, useMutation} from '@tanstack/react-query'
+// import {QUERIES} from '../../../../../../../_metronic/helpers'
+// import {useListView} from '../../core/ListViewProvider'
+// import {useQueryResponse} from '../../core/QueryResponseProvider'
+// import {deleteSelectedUsers} from '../../core/_requests'
 
-const UsersListGrouping = () => {
-  const {selected, clearSelected} = useListView()
-  const queryClient = useQueryClient()
-  const {query} = useQueryResponse()
+// const UsersListGrouping = () => {
+//   const {selected, clearSelected} = useListView()
+//   const queryClient = useQueryClient()
+//   const {query} = useQueryResponse()
 
-  const deleteSelectedItems = useMutation({
-    mutationFn: () => deleteSelectedUsers(selected),
-    onSuccess: () => {
-      // ✅ update detail view directly
-      queryClient.invalidateQueries({
-          queryKey: [`${QUERIES.USERS_LIST}-${query}`]
-      })
-      clearSelected()
-    }
-  });
+//   const deleteSelectedItems = useMutation({
+//     mutationFn: () => deleteSelectedUsers(selected),
+//     onSuccess: () => {
+//       // ✅ update detail view directly
+//       queryClient.invalidateQueries({
+//           queryKey: [`${QUERIES.USERS_LIST}-${query}`]
+//       })
+//       clearSelected()
+//     }
+//   });
 
-  return <div className='d-flex justify-content-end align-items-center'>
-    <div className='fw-bolder me-5'>
-      <span className='me-2'>{selected.length}</span> Selected
-    </div>
+//   return <div className='d-flex justify-content-end align-items-center'>
+//     <div className='fw-bolder me-5'>
+//       <span className='me-2'>{selected.length}</span> Selected
+//     </div>
 
-    <button
-      type='button'
-      className='btn btn-danger'
-      onClick={async () => await deleteSelectedItems.mutateAsync()}
-    >
-      Delete Selected
-    </button>
-  </div>
-}
+//     <button
+//       type='button'
+//       className='btn btn-danger'
+//       onClick={async () => await deleteSelectedItems.mutateAsync()}
+//     >
+//       Delete Selected
+//     </button>
+//   </div>
+// }
 
-export {UsersListGrouping}
+// export {UsersListGrouping}
