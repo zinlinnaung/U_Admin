bec5e3ec15455283efa864d79579f60ad4029229
zinlinnaung.bit <zinlinnaung.bit@gmail.com>
2025-11-01 11:22:24 +0630

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ important for Vercel
+  base: "/admin/", // ✅ ensures correct asset paths
   server: {
     port: 3081,
   },
   preview: {
-    allowedHosts: ["mypadmin.bitmyanmar.info"],
+    allowedHosts: ["mypadmin.bitmyanmar.info"], // ✅ your domain
   },
   build: {
     chunkSizeWarningLimit: 3000,
